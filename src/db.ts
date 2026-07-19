0ecaeb7f329d9ec5bca05def546eb3e66c2c337b
leanthelcolon <leanthelcolon@gmail.com>
2026-07-18 21:32:49 -0400

    fix: syntax error in db.ts

--- src/db.ts
+++ src/db.ts
@@ -15,7 +15,7 @@
 import { createClient, type SupabaseClient } from "@supabase/supabase-js";
 import "dotenv/config";
 
-const SUPABASE_URL = process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
+const SUPABASE_URL = process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY ?? "";
 
 export const supabase: SupabaseClient = createClient(
